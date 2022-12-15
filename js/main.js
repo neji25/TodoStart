6ebe096ba5b54af454e8cd0b9928322352be45ee
Vlad <myborvlad@gmail.com>
2022-12-15 22:28:33 +0300

    add HTML to LocalStorage

--- js/main.js
+++ js/main.js
@@ -4,8 +4,20 @@ const taskInput = document.querySelector('#taskInput')
 const tasksList = document.querySelector('#tasksList')
 const emptyList = document.querySelector('#emptyList')
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
+const tasks = []
+
+//Добавление задачи
+form.addEventListener('submit', addTask)
+
+//Удаление задачи
+tasksList.addEventListener('click', deleteTask)
+
+//Отмечаем задачу завершенной
+tasksList.addEventListener('click', doneTask)
+
+
+function addTask(event) {
+    event.preventDefault()
 
     //Достаем текст задачи из поля ввода
     const taskText = taskInput.value
@@ -35,4 +47,24 @@ form.addEventListener('submit', e => {
     if(tasksList.children.length > 1) {
         emptyList.classList.add('none')
     }
-})
+}
+
+function deleteTask(event) {
+    if(event.target.dataset.action !== 'delete') return
+
+    const parentNode = event.target.closest('.list-group-item')
+    parentNode.remove()
+
+    //Скрываем блок "Список пуст", если в списке есть задачи
+    if(tasksList.children.length == 1) {
+        emptyList.classList.remove('none')
+    }
+}
+
+function doneTask(event) {
+    if(event.target.dataset.action !== 'done') return
+
+    const parentNode = event.target.closest('.list-group-item')
+    const taskTitle = parentNode.querySelector('span')
+    taskTitle.classList.toggle('task-title--done')
+}
